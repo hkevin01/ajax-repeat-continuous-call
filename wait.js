daf4fdde32db9a4144395f56e13781defa9e7553
hkevin01 <kevin.hildebrand@gmail.com>
2017-03-14 14:24:28 -0400

    Create wait.js

---
+++ wait.js
@@ -0,0 +1,32 @@
+function waitForMSG(filename){
+	
+	 $.ajax({
+		url: urlPath + "obsResult.php", 
+		dataType: 'json',
+		async: true,
+		timeout:150000,
+		cache: false,
+		data: { filename: filename },
+		success: function(data) { 
+			if(data == null){
+				setTimeout(function(){ waitForMSG(filename)}, 10000);
+			}
+			else{		
+				 if(parseInt(data.obs.length) == 0 ){    
+					  setDisplayTime();
+					  loading(false);
+					  setTimeout(function(){
+					  $.colorbox({href: urlPath + "nohazard.php", onLoad: function() { $('#cboxClose').show();},onClosed:function(){ $('#cboxClose').hide();}});
+					  }, 1000);
+				 }
+				 else{
+					createMap(data);
+					loading(false);
+				 }
+			}
+		},
+		error: function(data){
+			setTimeout(function(){ waitForMSG(filename)}, 10000);
+		}
+	}); //end ajax
+}//return filename
